4b71b9f3522cdd13966062ae0798d081d9811907
Haydn Welch <hadz_94@hotmail.co.uk>
2021-03-13 11:03:58 +0000

    tidying up and adding webpack

--- src/bouncing-ball.js
+++ src/bouncing-ball.js
@@ -7,33 +7,42 @@ const g = 0.981; // gravity
 const fac = 0.8; // velocity reduction factor per bounce
 const radius = 20; // ball radius
 
-// initialise position and velocity of ball
-
 const state = [];
 
 function drawBall(ball, oldXPos, oldYPos) {
-  // with (context){
-  context.clearRect(oldXPos - radius, oldYPos - radius, radius * 2, radius * 2); // clear canvas
+  context.clearRect(oldXPos - radius, oldYPos - radius, radius * 2, radius * 2); // clear ball arcs
   context.beginPath();
-  context.arc(ball.x, ball.y, radius, 0, 2 * Math.PI, true);
+  context.arc(ball.x, ball.y, radius, 0, 2 * Math.PI, true); // draw ball
   context.closePath();
   context.fill();
-  // };
 }
 
 function update(ball) {
   const xToRemove = ball.x;
   const yToRemove = ball.y;
-  const newBall = ball;
+  const newBall = ball
   // update velocity
   newBall.vy += g; // gravity
 
   // update position
   newBall.x += newBall.vx;
   newBall.y += newBall.vy;
+
+  // bounce the ball off each wall
+  if (newBall.x - radius / 2 < 0 && newBall.vx < 0) {
+    newBall.vx = -newBall.vx;
+  }
+  if (newBall.x + radius / 2 > canvas.width && newBall.vx > 0) {
+    newBall.vx = -newBall.vx;
+  }
+  if (newBall.y - radius / 2 < 0 && newBall.vy < 0) {
+    newBall.vy = -newBall.vy;
+  }
+
   // handle bouncing
   if (newBall.y > canvas.height - radius) {
     newBall.y = canvas.height - radius;
+    // stop ball after x bounces
     if (newBall.remainingBounces > 0) {
       newBall.vy *= -fac;
       newBall.remainingBounces -= 1;
@@ -48,8 +57,10 @@ function update(ball) {
 
 // ensure that code does not run before page has loaded
 window.onload = document.getElementById('canvas').addEventListener('click', (e) => {
-  let vx = Math.floor(Math.random() * 15);
-  let vy = Math.floor(Math.random() * 15);
+  // setup random velocities
+  let vx = Math.floor(Math.random() * 10);
+  let vy = Math.floor(Math.random() * 10);
+  // setup random direction
   const randomDir = Math.floor(Math.random() * 2);
   if (randomDir === 0) {
     vx = -vx;
@@ -57,13 +68,15 @@ window.onload = document.getElementById('canvas').addEventListener('click', (e) 
   if (randomDir === 0) {
     vy = -vy;
   }
+  // tracking state of latest ball
   state.push({
     x: e.offsetX,
     y: e.offsetY,
     vx,
     vy,
-    remainingBounces: 30,
+    remainingBounces: 50,
   });
+  // if this is the first ball, kick off the updating of state
   if (state.length === 1) {
     setInterval(() => {
       for (let i = 0; i < state.length; i += 1) {
